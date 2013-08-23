f5ddf89dca57888ca61a8a5709ee26e6c63e5493
Gary Sieling <gary.sieling@gmail.com>
2013-08-22 21:59:27 -0400

    Support args

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-console.log('Loading a web page');
-var page = require('webpage').create();
-var url = 'http://www.phantomjs.org/';
-page.open(url, function (status) {
-    //Page is loaded!
-    phantom.exit();
-});
+var system = require('system');
+if (system.args.length === 1) {
+    console.log('Usage: phantomjs main.js examples/tests.pdf');
+} else {
+    system.args.forEach(function (arg, i) {
+            console.log(i + ': ' + arg);
+    });
+}
+phantom.exit();
 
