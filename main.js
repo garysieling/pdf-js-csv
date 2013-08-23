731a0fa755d18d7544d50a591890ce78eeb02581
Gary Sieling <gary.sieling@gmail.com>
2013-08-22 21:43:23 -0400

    Complete moves

--- main.js
+++
@@ -1,8 +0,0 @@
-console.log('Loading a web page');
-var page = require('webpage').create();
-var url = 'http://www.phantomjs.org/';
-page.open(url, function (status) {
-    //Page is loaded!
-    phantom.exit();
-});
-
