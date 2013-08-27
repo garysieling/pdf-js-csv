b50692cc2a6e6a9f0a0484c25248cf09ef36c5e4
Gary Sieling <gary.sieling@gmail.com>
2013-08-26 21:27:10 -0400

    Interprocess communication between PhantomJS and html

--- main.js
+++ main.js
@@ -1,10 +1,51 @@
-var system = require('system');
+var fs = require('fs'),
+    system = require('system');
+
 if (system.args.length === 1) {
-    console.log('Usage: phantomjs main.js examples/tests.pdf');
+  console.log('Usage: phantomjs main.js examples/tests.pdf');
+  phantom.exit();
 } else {
-    system.args.forEach(function (arg, i) {
-            console.log(i + ': ' + arg);
-    });
+  var pdf = system.args[1];
+
+  function utf8_to_b64( str ) {
+    return window.btoa(unescape(encodeURIComponent(str)));
+  }
+
+  var content = '',
+    f = null;
+
+  try {
+    f = fs.open(system.args[1], "r");
+    content = f.read();
+  } catch (e) {
+    console.log(e);
+    if (f !== null) {
+      f.close();
+    }
+    phantom.exit();
+  }
+
+  if (f) {
+    f.close();
+  }
+  
+  var data = utf8_to_b64(content);
+
+  var page = require('webpage').create();
+  var url = 'lib/index.html';
+  
+  page.onCallback = function(data) {
+    console.log(data);
+    phantom.exit();
+  }
+
+  page.open(url, function (status) {
+    page.evaluate(function(data) {
+      document.getElementById("pdf").innerText = data;
+      window.callPhantom("Sample: " + data.substring(0, 100));
+    }, data);
+    console.log("Finished");
+  });
+
 }
-phantom.exit();
 
