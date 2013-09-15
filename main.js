65be5e63ab59ac25b05afebd9ecc3d4f8eab31c8
Gary Sieling <gary.sieling@gmail.com>
2013-09-15 19:31:08 -0400

    Handle errors better

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 var pdf2csv = require('pdf2csv');
-pdf2csv.pdf2csv(process.argv[2], process.argv[3]);
+if( Object.prototype.toString.call(pdf2csv) == '[object String]' ) {
+  console.log(pdf2csv);
+} else {
+  pdf2csv.pdf2csv(process.argv[2], process.argv[3]);
+}
