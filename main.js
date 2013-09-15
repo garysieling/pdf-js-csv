461e2c8237b5afcdc77e416ce07f15c22996a941
Gary Sieling <gary.sieling@gmail.com>
2013-09-15 19:40:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
 var pdf2csv = require('pdf2csv');
-if( Object.prototype.toString.call(pdf2csv) == '[object String]' ) {
-  console.log(pdf2csv);
-} else {
-  pdf2csv.pdf2csv(process.argv[2], process.argv[3]);
-}
